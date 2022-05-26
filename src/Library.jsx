d5011b289c0cc36f7cadc9ea5eba97ef6e14b5f8
Bastian Vergara <bastian@rasen.cl>
2022-05-25 23:58:44 -0400

    Adding new exports

--- src/Library.jsx
+++ src/Library.jsx
@@ -6,3 +6,10 @@ export { default as ButtonTop } from "./ButtonTop";
 export { default as HotelShowcase } from "./HotelShowcase";
 export { default as HotelCard } from "./HotelShowcase/HotelCard";
 export { default as Footer } from "./Footer"
+export { default as ContactUs } from "./Footer/ContactUs"
+export { default as LeftLinks } from "./Footer/LeftLinks"
+export { default as RightLinks } from "./Footer/RightLinks"
+export { default as Sponsored } from "./Footer/Sponsored"
+export { default as SubFooter } from "./Footer/SubFooter"
+export { default as Topics } from "./Footer/Topics"
+export { default as WhereWeAre } from "./Footer/WhereWeAre"
