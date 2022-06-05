69ee058d84c85b8aa1d9ceee5d6b9430e6dea19d
Bastian Vergara <bastian@rasen.cl>
2022-06-05 13:37:05 -0400

    Removing bootstrap from header it will added to ThemeProvider.jsx

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,69 +1,67 @@
-import { Button, Col, Row } from "react-bootstrap";
-import Menu from "./Menu";
-import MenuItem from "./Menu/MenuItem";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./../index.css";
-import "./Header.css";
-const Header = ({
-  logo,
-  menuItems,
-  loggedIn,
-  loggedUserName,
-  onClickLogo,
-  onClickLoginButton,
-  onClickLogoutButton,
-}) => {
-  const handleClickLogo = () => {
-    if (onClickLogo) onClickLogo();
-  };
-  return (
-    <header className="light_header">
-      <div className="container">
-        <Row>
-          <Col>
-            <div className="menu">
-              <div className="brand-logo">
-                <img
-                  src={logo}
-                  alt="brand-logo"
-                  className="img-fluid lazyload"
-                  onClick={handleClickLogo}
-                />
-              </div>
-              {menuItems && (
-                <Menu>
-                  <>
-                    {menuItems.map((item, index) => (
-                      <MenuItem key={index} children={item.node} onClick={item.onClick} />
-                    ))}
-                  </>
-                </Menu>
-              )}
-              <ul className="header-right">
-                <li className={""}>
-                  <span className={"custom-header-item"}>
-                    {loggedIn && loggedUserName}
-                  </span>
-                </li>
+import { Button, Col, Row } from 'react-bootstrap';
+import Menu from './Menu';
+import MenuItem from './Menu/MenuItem';
+import './../index.css';
+import './Header.css';
 
-                <li className="user">
-                  {loggedIn && (
-                    <Button variant="dark" onClick={onClickLogoutButton}>
-                      Logout
-                    </Button>
-                  )}
-                  {!loggedIn && (
-                    <Button variant="dark" onClick={onClickLoginButton}>
-                      Login
-                    </Button>
-                  )}
-                </li>
-              </ul>
-            </div>
-          </Col>
-        </Row>
-      </div>
-    </header>
-  );
+const Header = ({
+	logo,
+	menuItems,
+	loggedIn,
+	loggedUserName,
+	onClickLogo,
+	onClickLoginButton,
+	onClickLogoutButton,
+}) => {
+	const handleClickLogo = () => {
+		if (onClickLogo) onClickLogo();
+	};
+	return (
+		<header className="light_header">
+			<div className="container">
+				<Row>
+					<Col>
+						<div className="menu">
+							<div className="brand-logo">
+								<img
+									src={logo}
+									alt="brand-logo"
+									className="img-fluid lazyload"
+									onClick={handleClickLogo}
+								/>
+							</div>
+							{menuItems && (
+								<Menu>
+									<>
+										{menuItems.map((item, index) => (
+											<MenuItem key={index} children={item.node} onClick={item.onClick} />
+										))}
+									</>
+								</Menu>
+							)}
+							<ul className="header-right">
+								<li className={''}>
+									<span className={'custom-header-item'}>{loggedIn && loggedUserName}</span>
+								</li>
+
+								<li className="user">
+									{loggedIn && (
+										<Button variant="dark" onClick={onClickLogoutButton}>
+											Logout
+										</Button>
+									)}
+									{!loggedIn && (
+										<Button variant="dark" onClick={onClickLoginButton}>
+											Login
+										</Button>
+									)}
+								</li>
+							</ul>
+						</div>
+					</Col>
+				</Row>
+			</div>
+		</header>
+	);
 };
 export default Header;
