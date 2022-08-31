170ddfa206c4a7e945ee7701db3b883de20f8edf
Bastian Vergara <bastian@rasen.cl>
2022-08-31 01:59:01 -0400

    Change radius

--- src/Step/styled.jsx
+++ src/Step/styled.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const StepsContainer = styled.div`
 	margin-top: 20px;
@@ -6,7 +6,7 @@ const StepsContainer = styled.div`
 	flex-wrap: wrap;
 	justify-content: space-between;
 	align-items: center;
-	border-radius: 2.5rem 2.5rem 2.5rem 2.5rem;
+	border-radius: 0.3rem 0.3rem 0.3rem 0.3rem;
 	box-shadow: 0 0.5rem 1rem #00000026;
 	background-color: ${(props) => {
 		return props.theme ? props.theme.colors.primary : '#ffffff';
