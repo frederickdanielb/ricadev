69234dcf572bc4b93c1b3bf935480daaad4d649b
Bastian Vergara <bastian@rasen.cl>
2022-10-03 00:39:48 -0300

    Fix styled in component

--- src/Nav/NavItem/NavItem.jsx
+++ src/Nav/NavItem/NavItem.jsx
@@ -1,29 +1,6 @@
-import styled from 'styled-components';
+import { TextContainer } from './styled';
 
 const NavItem = ({ onClick, children }) => {
-	const TextContainer = styled.button`
-    font-family: Nunito, sans-serif;
-    font-weight: bold;
-    text-transform: uppercase;
-    background: none;
-    border: none;
-    cursor: pointer;
-    padding: 0;
-    margin-top: 10px;
-    text-align: left;
-    margin-left: 15px;
-    margin-right: 15px;
-    font-size: 16px;
-    color: #000;
-    border-bottom: 2px solid  ${props =>{ return props.theme ? props.theme.colors.primary: '#ffffff'}};
-    padding-bottom: 5px;
-    padding-left: 10px;
-    padding-right: 10px;
-    &:hover {
-        border-bottom: 2px solid  ${props =>{ return props.theme ? props.theme.colors.primary: '#ffffff'}};
-        color:  ${props =>{ return props.theme ? props.theme.colors.primary: '#ffffff'}};
-    }
-`;
 	return <TextContainer onClick={onClick}>{children}</TextContainer>;
 };
 export default NavItem;
